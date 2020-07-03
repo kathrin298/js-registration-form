acf850f3eb48eb1665aaa1a4dca618bc1d6d6f6c
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-07-03 17:02:27 +0200

    added controlled components

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 
-class Form extends Component {
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movingDate: '',
+      postcode: 'postcode'
+    }
+  }
+
   handleSubmit = () => {
     // print results to console as JSON
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      movingDate: event.target.movingDate,
+      postcode: event.target.postcode
+    });
   }
 
   render() {
     return (
       <form className="form-group" onSubmit={this.handleSubmit} >
         <label htmlFor="m-date">Date of moving</label>
-        <input className="form-control" type="text" id="m-date" name="m-date"></input>
+        <input className="form-control" type="text" id="m-date" name="m-date" value={this.state.movingDate} onChange={this.handleChange} ></input>
 
         <label htmlFor="m-date">into the apartment in</label>
-        <input className="form-control" type="text" id="m-date" name="m-date" placeholder="postcode"></input>
+        <input className="form-control" type="text" id="m-date" name="m-date" onChange={this.handleChange} placeholder="postcode"></input>
 
         <label htmlFor="m-date">Name and address of the landlord</label>
         <input className="form-control" type="text" id="m-date" name="m-date"></input>
