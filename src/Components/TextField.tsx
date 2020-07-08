0fce6f01786dc5a790d21cfac79edf6f410a0043
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-07-08 16:00:49 +0200

    cleaned up comments

--- src/Components/TextField.tsx
+++ src/Components/TextField.tsx
@@ -12,13 +12,6 @@ type Props = {
 }
 
 class TextField extends React.Component<Props, {}> {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     value: ""
-  //   }
-  // }
-
   render() {
     return(
       <div>
